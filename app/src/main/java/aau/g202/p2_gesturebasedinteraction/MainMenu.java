82c55d875b424a1688c4d760b0ffc48a65283d2f
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2018-04-26 14:23:54 +0200

    1

---
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/MainMenu.java
@@ -0,0 +1,18 @@
+package aau.g202.p2_gesturebasedinteraction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import static aau.g202.p2_gesturebasedinteraction.R.layout.activity_mainmenu;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_mainmenu);
+        setContentView(R.layout.activity_main);
+    }
+
+}
