02724ba0fbd6fd5a8a69a7de2e638ee9be777b45
Revinz <revinz1414@gmail.com>
2018-04-27 11:33:26 +0200

    v0.0.1

---
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/ScrollMode.java
@@ -0,0 +1,12 @@
+package aau.g202.p2_gesturebasedinteraction;
+
+/**
+ * Created by Revinz on 27-Apr-18.
+ */
+
+public class ScrollMode extends ControlMode {
+
+    public void Update() {
+        //Update whatever you need updated in here
+    }
+}
