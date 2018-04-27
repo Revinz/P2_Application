b6e9b32385ddf5d9d2bbd7be556876ccd59ab16e
Revinz <revinz1414@gmail.com>
2018-04-27 11:30:40 +0200

    v0.1

---
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/SelectMode.java
@@ -0,0 +1,27 @@
+package aau.g202.p2_gesturebasedinteraction;
+
+/**
+ * Created by Revinz on 27-Apr-18.
+ */
+
+public class SelectMode extends ControlMode {
+
+    float posX;
+    float posY;
+
+    //image cursor;
+
+
+    void update(){}
+
+    void select(){}
+
+    void longSelect(){}
+
+    void drag(){}
+
+    public void Update() {
+
+    }
+
+}
