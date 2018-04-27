df1f01613855a1e7ce56d55b75f9d5c29efd8ac9
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2018-04-27 14:51:55 +0200

    2

--- app/src/main/java/aau/g202/p2_gesturebasedinteraction/MainMenu.java
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/MainMenu.java
@@ -2,7 +2,6 @@ package aau.g202.p2_gesturebasedinteraction;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
 
 import static aau.g202.p2_gesturebasedinteraction.R.layout.activity_mainmenu;
 
