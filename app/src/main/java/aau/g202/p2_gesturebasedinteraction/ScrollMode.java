1a6ffb22332f0810316b4bc45b0ec2ae933fe09e
JannikPedersen <jclp17@student.aau.dk>
2018-04-30 09:24:01 +0200

    1.2 version
    needs fix

--- app/src/main/java/aau/g202/p2_gesturebasedinteraction/ScrollMode.java
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/ScrollMode.java
@@ -1,5 +1,4 @@
 //Pseudcode
-
 /*
 *make a fake touch that acts like a finger swiping up or down*
 check if the scrollMode is true or false;
@@ -20,17 +19,29 @@ import android.view.MotionEvent;
 
 public class ScrollMode extends ControlMode {
 
-        //the time (in ms) when the user originally pressed down to start a stream of position events.
-        // This must be obtained from SystemClock.uptimeMillis().
-        public long downTime = SystemClock.uptimeMillis();
-        //The time (in ms) when this specific event was generated.
-        // This must be obtained from SystemClock.uptimeMillis().
-        public long eventTime = SystemClock.uptimeMillis();
-        //this is done to make the fake touch scroll from the middle of the screen.
-        int x = width/2;
-        int y = height/2;
 
-        //The kind of action being performed -- one of either ACTION_DOWN,
+
+    public void Update() {
+        //Update whatever you need updated in here
+
+        if (currMode != Mode.SCROLLMODE){
+            return;
+        }
+    }
+
+
+    //the time (in ms) when the user originally pressed down to start a stream of position events.
+    // This must be obtained from SystemClock.uptimeMillis().
+    long downTime = SystemClock.uptimeMillis();
+    //The time (in ms) when this specific event was generated.
+    // This must be obtained from SystemClock.uptimeMillis().
+    long eventTime = SystemClock.uptimeMillis();
+    //this is done to make the fake touch scroll from the middle of the screen.
+    int x = width/2;
+
+        int y = height/2;
+    //The kind of action being performed -- one of either ACTION_DOWN,
+
         // ACTION_MOVE, ACTION_UP, or ACTION_CANCEL.
         @Override public boolean onTilt( MotionEvent event) {
             switch (event.getAction()) {
@@ -47,15 +58,11 @@ public class ScrollMode extends ControlMode {
             }
         }
 
+    MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, x, y, metaState);
 
-        MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, x, y, metaState);
 view.onTouchEvent(event);
 
 
     }
 
-
-    public void Update() {
-        //Update whatever you need updated in here
-    }
 }
