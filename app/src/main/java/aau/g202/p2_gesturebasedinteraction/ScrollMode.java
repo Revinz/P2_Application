5b538a0883ed5155ecc20ce40ff1fbbc4debddb2
JannikPedersen <jclp17@student.aau.dk>
2018-04-30 10:16:35 +0200

    1.3 version
    I have know idea what to fix things now

--- app/src/main/java/aau/g202/p2_gesturebasedinteraction/ScrollMode.java
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/ScrollMode.java
@@ -20,49 +20,46 @@ import android.view.MotionEvent;
 public class ScrollMode extends ControlMode {
 
 
-
     public void Update() {
         //Update whatever you need updated in here
 
-        if (currMode != Mode.SCROLLMODE){
+        if (currMode != Mode.SCROLLMODE)
             return;
-        }
-    }
+
+        float y = Accelerometer.getY() * -1;
+        float x = Accelerometer.getX() * -1;
 
 
-    //the time (in ms) when the user originally pressed down to start a stream of position events.
-    // This must be obtained from SystemClock.uptimeMillis().
-    long downTime = SystemClock.uptimeMillis();
-    //The time (in ms) when this specific event was generated.
-    // This must be obtained from SystemClock.uptimeMillis().
-    long eventTime = SystemClock.uptimeMillis();
-    //this is done to make the fake touch scroll from the middle of the screen.
-    int x = width/2;
+        //the time (in ms) when the user originally pressed down to start a stream of position events.
+        // This must be obtained from SystemClock.uptimeMillis().
+        long downTime = SystemClock.uptimeMillis();
+        //The time (in ms) when this specific event was generated.
+        // This must be obtained from SystemClock.uptimeMillis().
+        long eventTime = SystemClock.uptimeMillis();
 
-        int y = height/2;
-    //The kind of action being performed -- one of either ACTION_DOWN,
+        //The kind of action being performed -- one of either ACTION_DOWN,
 
         // ACTION_MOVE, ACTION_UP, or ACTION_CANCEL.
-        @Override public boolean onTilt( MotionEvent event) {
+        @Override public boolean action (MotionEvent event){
             switch (event.getAction()) {
                 case MotionEvent.ACTION_MOVE:
-                    x = TiltLevelX + (int) (event.getRawX() );
-                    y = TiltLevelY + (int) (event.getRawY() );
+                    x = TiltLevelX + (int) (event.getRawX());
+                    y = TiltLevelY + (int) (event.getRawY());
                     break;
                 //should make it stop scrolling by pressing a button
                 case MotionEvent.ACTION_CANCEL:
-                    if (press button == true) {
+                    if (press button == true){
                     //or false?
                     onTilt = 0;
                 }
             }
         }
 
-    MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, x, y, metaState);
+        MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, x, y, metaState);
 
-view.onTouchEvent(event);
+        view.onTouchEvent(event);
 
 
     }
-
+}
 }
