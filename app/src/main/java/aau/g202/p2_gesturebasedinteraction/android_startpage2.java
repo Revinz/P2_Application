44a18dbcfebc0d313fa926cf5da019d4ececa83a
Revinz <revinz1414@gmail.com>
2018-05-07 20:29:26 +0200

    Made it able to work with now

---
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/android_startpage2.java
@@ -0,0 +1,13 @@
+package aau.g202.p2_gesturebasedinteraction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class android_startpage2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_startpage2);
+    }
+}
