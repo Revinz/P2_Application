400f4d448e16c8785d707a354dc718025b475587
JannikPedersen <jclp17@student.aau.dk>
2018-05-08 13:57:23 +0200

    version4
    something is wrong but almost done

--- app/src/main/java/aau/g202/p2_gesturebasedinteraction/android_startpage2.java
+++ app/src/main/java/aau/g202/p2_gesturebasedinteraction/android_startpage2.java
@@ -9,5 +9,32 @@ public class android_startpage2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_startpage2);
-    }
+
+        setContentView(R.layout.content_layout_id);
+
+
+        //to facebook
+        final button buttun3 = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+                setContentView(R.layout.content_layout_id);
+
+                //to game
+                final Button button2 = findViewById(R.id.button_id);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Code here executes on main thread after user presses button
+                    }
+
+                        //to app
+                        setContentView(R.layout.content_layout_id);
+
+                        final Button button = findViewById(R.id.button_id);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                // Code here executes on main thread after user presses button
+                            }
+            }
 }
